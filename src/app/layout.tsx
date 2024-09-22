4f20d6043adbceaccd8b95196e4896297815f844
programarreza <programarreza@gmail.com>
2024-09-23 01:34:18 +0600

    folder and file structure setup done

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Apollo Gears",
+  description: "Next Level Riding Sharing Service",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={` antialiased`}>
+        {children}
+        Footer
+      </body>
+    </html>
+  );
+}
